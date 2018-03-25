f8f85ce39b9dd03568d81a451f0d9f24e2782fbb
ravrik <conver3510@gmail.com>
2018-03-25 16:35:04 +0900

    パーセプトロン仮実装

--- BinClass.py
+++ BinClass.py
@@ -56,3 +56,24 @@ class perseptron:
         self.eta = eta
         self.itr = itr
         self.epocsize = epocsize
+
+    def fit(self, x, y):
+        self.w = np.random.rand(x.shape[1]+1, 1)
+        x = np.hstack([np.ones([x.shape[0], 1]), x])
+        y = np.where(y == 0, -1, 1)
+
+        def conv(x):
+            return np.where(np.dot(x, self.w) > 0, 1, -1)
+
+        for i in range(self.itr):
+            epoc = list(np.random.choice(range(x.shape[0]), self.epocsize))
+            for tmp_idx in epoc:
+                tmp_x = x[tmp_idx, :]
+                tmp_y = y[tmp_idx, :]
+
+                grad = (self.eta * tmp_y * tmp_x).reshape(tmp_x.shape[0], 1)
+                self.w = np.where(conv(tmp_x) == tmp_y, self.w, self.w + grad)
+
+    def pred(self, x):
+        x = np.hstack([np.ones([x.shape[0], 1]), x])
+        return np.where(np.dot(x, self.w) > 0, 1, 0)
