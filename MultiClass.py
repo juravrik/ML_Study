f1e3e51a4c3b190a588c773287414cf6d4d14430
ravrik <conver3510@gmail.com>
2018-06-17 00:01:30 +0900

    マハラノビス距離

--- MultiClass.py
+++ MultiClass.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class COS:
+class Cos:
     '''
     cos類似度によるマルチクラス識別器
     '''
@@ -16,4 +16,22 @@ class COS:
 
             d = dict(zip(self.mean_vec.keys(), list(map(_cossim, self.mean_vec.values()))))
             l.append(max(d.items(), key=lambda x:x[1])[0])
+        return np.array(l)
+
+
+class Maharanobis:
+    def fit(self, X, y):
+        self.mean_vec = {i:np.mean(X[np.where(y==i)[0], :], axis=0) for i in np.unique(y)}
+
+    def predict(self, x):
+        l = []
+
+        c = np.cov(x.T)
+        for i in x:
+            def _maha(a):
+                t = np.reshape((i-a), ((i-a).shape[0], 1))
+                return np.sqrt(np.dot(np.dot(t.T, np.linalg.inv(c)), t))
+
+            d = dict(zip(self.mean_vec.keys(), list(map(_maha, self.mean_vec.values()))))
+            l.append(min(d, key=d.get))
         return np.array(l)
